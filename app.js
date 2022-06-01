43247f372e9d01d25155148aaae47a29b64b3d51
yousunzoo <102499959+yousunzoo@users.noreply.github.com>
2022-06-01 15:46:06 +0900

    addEventListener 학습

--- app.js
+++ app.js
@@ -26,10 +26,39 @@
  * title.addEventListener("event", function)
  */
 
-const title = document.querySelector(".hello h1");
+const h1 = document.querySelector(".hello h1");
 function handleTitleClick(){
-  title.style.color = "blue";
+  h1.style.color = "blue";
   console.log("title was clicked!")
 }
 
-title.addEventListener("click", handleTitleClick)
+function handleMouseEnter(){
+  h1.innerText = "mouse is here!"
+};
+
+function handleMouseLeave(){
+  h1.innerText = "mouse is gone"
+};
+
+function handleWindowResize(){
+  document.body.style.backgroundColor = "tomato";
+};
+
+function handleWindowCopy(){
+  alert("copier!");
+};
+function handleWindowOffline(){
+  alert("SOS no WIFI")
+}
+function handleWindowOnline(){
+  alert("connected")
+}
+
+h1.addEventListener("click", handleTitleClick);
+h1.addEventListener("mouseenter", handleMouseEnter);
+h1.addEventListener("mouseleave", handleMouseLeave);
+
+window.addEventListener("resize", handleWindowResize);
+window.addEventListener("copy", handleWindowCopy);
+window.addEventListener("offline", handleWindowOffline);
+window.addEventListener("online",handleWindowOnline )
