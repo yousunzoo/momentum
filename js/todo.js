c5850bcb652025984d0d8be251bf400612ea5497
yousunzoo <102499959+yousunzoo@users.noreply.github.com>
2022-06-04 23:00:14 +0900

    button 이용해서 li 삭제

--- js/todo.js
+++ js/todo.js
@@ -2,11 +2,20 @@ const todoForm = document.getElementById('todo-form');
 const todoInput = todoForm.querySelector("input");
 const todoList = document.getElementById('todo-list');
 
+const deleteToDo = function(event){
+const li = event.target.parentElement;
+li.remove();
+}
+
 function paintTodo(newTodo){
  const li = document.createElement("li");
  const span = document.createElement("span");
- li.appendChild(span);
  span.innerText = newTodo;
+ const button = document.createElement("button");
+  button.innerText = "❌";
+  button.addEventListener("click", deleteToDo)
+ li.appendChild(span);
+ li.appendChild(button);
  todoList.appendChild(li);
 }
 
