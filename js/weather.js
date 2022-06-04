6f91b6606f490db4ad2afdb2efc6815784a04bad
yousunzoo <102499959+yousunzoo@users.noreply.github.com>
2022-06-05 00:17:15 +0900

    weather API

--- js/weather.js
+++ js/weather.js
@@ -1,8 +1,17 @@
+const API_KEY = "05fb53ddaab38a01e78274d0c0b38763";
 
 function onGeoOk(position){
   const lat = position.coords.latitude;
-  const lng = position.coords.longitude;
-console.log("You live in", lat, lng)
+  const lon = position.coords.longitude;
+console.log("You live in", lat, lon);
+const url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${API_KEY}&units=metric`;
+fetch(url).then(response = reponse.json())
+.then(data => {
+  const weather = document.querySelector("#weather span:first-child");
+  const city = document.querySelector("#weather span:last-child");
+  city.innerText = data.name;
+  weather.innerText = data.weather[0].main;
+});
 };
 function onGeoError(){
   alert("Can't find you. No weather for you.")
