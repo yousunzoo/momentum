b17be15ac33ef1ac6f74826cea99bf5ffdaa3ec4
yousunzoo <102499959+yousunzoo@users.noreply.github.com>
2022-06-04 21:06:09 +0900

    random background

---
+++ js/background.js
@@ -0,0 +1,13 @@
+// createElement, appendChild
+
+const images = [
+  "image01.jpg", "image02.jpg", "image03.jpg","image04.jpg", "image05.jpg"
+];
+
+chosenImage = images[Math.floor(Math.random()*images.length)];
+
+const bgImage = document.createElement("img");
+
+bgImage.src = `img/${chosenImage}`;
+
+document.body.appendChild(bgImage)
