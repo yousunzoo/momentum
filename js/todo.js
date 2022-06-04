70b26a913efb13530f863edd386ce438386efaed
yousunzoo <102499959+yousunzoo@users.noreply.github.com>
2022-06-04 22:51:45 +0900

    ToDo list 에 li 추가

--- js/todo.js
+++ js/todo.js
@@ -2,11 +2,19 @@ const todoForm = document.getElementById('todo-form');
 const todoInput = todoForm.querySelector("input");
 const todoList = document.getElementById('todo-list');
 
+function paintTodo(newTodo){
+ const li = document.createElement("li");
+ const span = document.createElement("span");
+ li.appendChild(span);
+ span.innerText = newTodo;
+ todoList.appendChild(li);
+}
+
 function handleToDoSubmit (event){
   event.preventDefault();
   const newTodo = todoInput.value; 
   todoInput.value ="";
-
+  paintTodo(newTodo);
 }
 
 todoForm.addEventListener("submit", handleToDoSubmit)
