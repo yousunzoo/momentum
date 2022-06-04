a3e4467133a839f5a382c48b7251a95e39942474
yousunzoo <102499959+yousunzoo@users.noreply.github.com>
2022-06-04 20:43:22 +0900

    new Date()

--- js/clock.js
+++ js/clock.js
@@ -2,7 +2,10 @@
 // setTimeout --> 몇 초 뒤에 실행
 // .padStart(2, "0") --> string의 길이가 2가 되지 않는다면 앞에 0을 추가
 // .padEnd(2, "0") --> 문자 뒤에 0을 추가
-// date()는 숫자이므로 string(new Date())로 문자화시켜줘야 함
+// date()는 숫자이므로 String(new Date())로 문자화시켜줘야 함
+/**date를 new Date()로 선언하는 이유
+ * new를 선두에 두고 생성자 함수를 호출하면 instance object를 반환
+*/
 
 const clock = document.querySelector("h2#clock");
 
